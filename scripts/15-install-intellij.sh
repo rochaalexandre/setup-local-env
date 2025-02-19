3760a9458e3836a56ee5cbf2750df97d1f73bd4c
Alexandre Rocha <alexandrececchin@gmail.com>
2025-02-19 18:19:55 -0300

    Add new Script

---
+++ scripts/15-install-intellij.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define installation path
+INSTALL_DIR="$HOME/.local/intellij"
+
+# Create installation directory if it doesn't exist
+mkdir -p "$INSTALL_DIR"
+
+# Download and extract IntelliJ IDEA to the specified path
+echo "Downloading IntelliJ IDEA..."
+curl -L "https://download.jetbrains.com/product?code=IU&latest&distribution=linux" | tar xvz -C "$INSTALL_DIR" --strip 1
+
+echo "Installation complete. Run IntelliJ IDEA with: $INSTALL_DIR/bin/idea.sh"
